4e39c2a740262ac2cfb08595d3c349745dd932d6
Rafael Vidal <vidal@live.com>
2024-09-30 21:34:21 +0800

    Initial Commit

---
+++ gymmerApp/gymmerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  gymmerAppApp.swift
+//  gymmerApp
+//
+//  Created by Rafael Alves Vidal on 30/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct gymmerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
